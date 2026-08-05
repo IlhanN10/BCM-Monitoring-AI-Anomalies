e568010d9f7fd160c42a426172b5b35013b4e9d0
Ilhan Nuhic <ilhan.nuhic@itSchuleStuttgart1.onmicrosoft.com>
2026-08-06 00:46:01 +0200

    Repo Clean and test

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 BNI_IP = "192.168.1.1"
-BCM_PORT_ALIAS = "master1port2"
+BCM_PORT_ALIAS = "master1port1"
 
-TOKEN = "jd469lfjls7xu7g"
-SESSION_ID = "8ygzr2t149vzgdh"
+TOKEN = "mfibrx7rkt1sj62"
+SESSION_ID = "znul3md3v4s5nwa"
 
 BCM_URL = f"http://{BNI_IP}/iolink/v1/devices/{BCM_PORT_ALIAS}/processdata/value?format=byteArray"
 PORTS_URL = f"http://{BNI_IP}/iolink/v1/masters/1/ports"
